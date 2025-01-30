d176ed3f8de21ff2aba1182ca6d5fbf4715d4289
Aman Sagar <amansagar0307@gmail.com>
2025-01-30 19:57:29 +0530

    fix bugs

--- src/app.js
+++ src/app.js
@@ -34,6 +34,10 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the backend API!');
+});
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
